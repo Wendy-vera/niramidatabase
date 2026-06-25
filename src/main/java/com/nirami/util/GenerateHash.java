156369f33054e0097171856e3573f1fd89b1874e
wendy <wendyvanesavera@gmail.com>
2026-06-25 08:12:01 -0500

    manual de usuarios pendiente de actualizacion

--- src/main/java/com/nirami/util/GenerateHash.java
+++
@@ -1,8 +0,0 @@
-import org.mindrot.jbcrypt.BCrypt;
-
-public class GenerateHash {
-    public static void main(String[] args) {
-        String hash = BCrypt.hashpw("RosaFabio9014", BCrypt.gensalt(12));
-        System.out.println("HASH: " + hash);
-    }
-}
